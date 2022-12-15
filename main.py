868d750ea6cc732b00e7e8e92a9c9599b21e6686
Gennady Umikashvili <gennadyumikashvili@yandex.ru>
2022-12-15 13:26:43 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# pylint: disable=redefined-outer-name
+
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaverx
 
-from graphic_arts.start_game_banner import run_screensaver
+
 
 
 def attack(char_name: str, char_class: str) -> str:
